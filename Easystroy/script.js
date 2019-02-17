e74dfb5129a1c27696bb7f6385aa8203998f92f1
badcatman <bogdanslupachik68@gmail.com>
2019-02-17 22:53:35 +0200

    b

---
+++ Easystroy/script.js
@@ -0,0 +1,14 @@
+// var windowsSize=function(){
+
+// 	let w = document.documentElement.clientWidth;
+
+// 	if (w < 768) {
+// 		(".header > .container").removeClass("container").addClass("container-fluid");
+
+// 	}
+// 	else if (w > 768) {
+// 		(".header > .container-fluid").removeClass("container-fluid").addClass("container");
+// 	}
+
+// }; 
+// windowSize();
